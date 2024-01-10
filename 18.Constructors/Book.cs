e1e1b1c82f70bd3f25e7ea73ed39ede848e6a8f1
A253875 <A253875@LSBSA-65LG3J3>
2024-01-10 16:14:00 +0200

    Add project files.

---
+++ 18.Constructors/Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{   /*classes are for creating more complex data type, eg a book can have a title,
+     and pages which are different data types combined*/
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        //this method belowis called a constructor, it is called by default when a Book
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+            
+            
+            
+    }   
+    
+
+}
---
+++ 18.Constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+
+            Console.WriteLine(book1.title);
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book1.pages);
+
+            Console.ReadLine();
+
+        }
+    }
+}
